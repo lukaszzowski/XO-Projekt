682dbe70272c66af0fa02ee87d838084413addf8
Freeman84 <32610720+Freeman84@users.noreply.github.com>
2018-01-25 10:39:47 +0100

    game.h with googlestyle
    Final version with googlestyle

--- game.h
+++ game.h
@@ -2,42 +2,31 @@
 #define GAME_H
 #include <iostream>
 #include <limits>
-#include <stdio.h>      /* printf, fgets */
+#include <stdio.h> /* printf, fgets */
 #include <stdlib.h>
 
 using namespace std;
 
 int main();
 
-class game
-{
-    public:
-        void show_board();
-        //int choice();
-        void player_move();
-        bool check_winner();
-        char turn;
-        bool draw = false;
-        char board[3][3] = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
-
+class game {
+ public:
+  void show_board();
+  // int choice();
+  void player_move();
+  bool check_winner();
+  char turn;
+  bool draw = false;
+  char board[3][3] = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
 };
 
-class player : public game
-{
-    public:
-        void player_move();
-        char turn;
+class player : public game {
+ public:
+  void player_move();
+  char turn;
 };
 
+class player1 : public player {};
+class player2 : public player {};
 
-class player1 : public player
-{
-
-};
-class player2 : public player
-{
-
-};
-
-
-#endif // GAME_H
+#endif  // GAME_H
